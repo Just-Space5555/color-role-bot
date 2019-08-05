1a682daf41a7f3e57cee728661f389a6c5b44635
Just-Space5555 <48643725+Just-Space5555@users.noreply.github.com>
2019-08-05 16:31:53 +0300

    yarik

---
+++ script.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith('+')) {
+    if(msg.author != client.user){
+        var messageSplited = msg.content.split(' ');
+        if(messageSplited.length >= 2){
+            if(messageSplited[0] == "+color" && messageSplited[1].startsWith('#')){
+                msg.guild.createRole({
+                  name: msg.author.id,
+                  color: messageSplited[1],
+                });
+                        if(msg.guild.roles.find("name", msg.author.id) != null){
+                            msg.guild.roles.find("name", msg.author.id).delete();
+                        }
+                        setTimeout(function(){
+                            var role = msg.guild.roles.find("name", msg.author.id);
+                            msg.member.addRole(role.id);
+                        }, 1000)
+                        msg.channel.send(":white_check_mark: **Successfully changed color** of user " + msg.author + " to " + messageSplited[1] + "!")
+                       
+            }
+        }
+    }
+  }
+});
+
+client.login(env.token);
